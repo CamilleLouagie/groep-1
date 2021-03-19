cb309de26c60a06c6e45f21f84710e6e953db16a
Staf Rys <staf.rys@student.kuleuven.be>
2021-03-19 17:03:01 +0100

    Nieuwe implementaties

---
+++ Programmeren/kruispunt.py
@@ -0,0 +1,14 @@
+
+def kruispunt(nummer):
+    if nummer == 1:     #Check op berichten halverwege algoritme
+        pass
+    elif nummer == 2:
+        pass
+    elif nummer == 3:
+        pass
+    elif nummer == 4:
+        pass
+    elif nummer == 5:
+        pass
+
+    # ...
