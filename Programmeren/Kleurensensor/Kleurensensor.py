a1257f8795a02e11a49bee9a37b4b8c3a7e706ec
MeerschmanOtto <otto.meerschman@student.kuleuven.be>
2021-03-25 18:18:22 +0100

    kleursensor

--- Programmeren/Kleurensensor/Kleurensensor.py
+++ Programmeren/Kleurensensor/Kleurensensor.py
@@ -14,22 +14,22 @@ import busio
 GPIO.setmode(GPIO.BOARD)
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_tcs34725.TCS34725(i2c)
-def functie():# functie die zegt wanneer moet detecteren
+def functie():# functie die zegt wanneer moet detecteren = wanneer aan stoplijn
 
     return  # true of false
 
-def detectiekleuren(sensor):
-    while true:
-        r, g, b, c = sensor.get_raw_data() # of rgb_golor_bytes
-        if r>b and r>g:
-            if b<5:
-            k= 'rood'
 
-        if g>r and b<g and g>20:
-            k= 'groen'
+def detectiekleuren(sensor, functie):
+    while functie == true:
+        r, g, b, c = sensor.get_raw_data()  # of rgb_golor_bytes
+
+        if g > r and g > b and g > c:
+            time.sleep(1)
+            if g < 50: #kan aangepast worden
+                time.sleep(1)
+                if g > r and g > b and g > c:
+                    return true
+
         else:
-            k='groen'
-
-    return k
-
+            return false
          
