3dd98615122e14dce733849e43d79832ea091557
Ruben Leenknecht <ruben.leenknecht@student.kuleuven.be>
2021-03-26 16:18:59 +0100

    a

--- Programmeren/MotorControl/wiringmotor.py
+++ Programmeren/MotorControl/wiringmotor.py
@@ -1,5 +1,8 @@
+
+
 import RPi.GPIO as GPIO
 import time
+import wiringpi
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -15,6 +18,32 @@ BBackwardsPin = 31
 
 # EnablePinA =
 # EnablePinB =
+
+
+LED  = 1 # gpio pin 12 = wiringpi no. 1 (BCM 18)
+
+# Initialize PWM output for LED
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(LED, 2)     # PWM mode
+wiringpi.pwmWrite(LED, 0)    # OFF
+
+# Set LED brightness
+def led(led_value):
+    wiringpi.pwmWrite(LED,led_value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def motorinitialisatie():
